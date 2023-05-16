928e8a7511b03be0b2ff926f74dbf7a6d089f7a7
Ivan Koshkin <ivan.koshkin35@gmail.com>
2023-05-16 17:13:40 +0300

    added the ability to download a list of orders as a CSV file

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,6 @@
+import csv
+import datetime
+from django.http import HttpResponse
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from .models import Order, OrderItem
@@ -19,6 +22,31 @@ def order_payment(obj):
 order_payment.short_description = 'Stripe payment'
 
 
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many \
+              and not field.one_to_many]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for odj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(odj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Export to CSV'
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
@@ -26,3 +54,4 @@ class OrderAdmin(admin.ModelAdmin):
                     order_payment, 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemLine]
+    actions = [export_to_csv]
