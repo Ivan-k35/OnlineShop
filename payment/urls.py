154b789922824c2a8f09167875e2d88ce53074cf
Ivan Koshkin <ivan.koshkin35@gmail.com>
2023-06-01 14:16:34 +0300

    fix some bugs

--- payment/urls.py
+++ payment/urls.py
@@ -1,11 +1,10 @@
-from django.utils.translation import gettext_lazy as _
 from django.urls import path
 from . import views
 
 app_name = 'payment'
 
 urlpatterns = [
-    path(_('process/'), views.payment_process, name='process'),
-    path(_('completed/'), views.payment_completed, name='completed'),
-    path(_('canceled/'), views.payment_canceled, name='canceled'),
+    path('process/', views.payment_process, name='process'),
+    path('completed/', views.payment_completed, name='completed'),
+    path('canceled/', views.payment_canceled, name='canceled'),
 ]
